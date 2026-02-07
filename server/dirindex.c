94882d8211f208e2c2d7b2c0cbe88a63647b9035
TheFallenWarrior <thefallenwarrior666@gmail.com>
2026-02-07 14:06:03 -0300

    Replace spaces with tabs in dirindex.c

--- server/dirindex.c
+++ server/dirindex.c
@@ -18,120 +18,120 @@
 
 // Enviar um chunk usando chunked encoding
 int sendChunk(int sockfd, const char *data, size_t len){
-    char header[32];
-    int headerLen = snprintf(header, sizeof(header), "%zx\r\n", len);
+	char header[32];
+	int headerLen = snprintf(header, sizeof(header), "%zx\r\n", len);
 
-    if(write(sockfd, header, headerLen) != headerLen) return 1;
-    if(len > 0 && write(sockfd, data, len) != (ssize_t)len) return 1;
-    if(write(sockfd, "\r\n", 2) != 2) return 1;
+	if(write(sockfd, header, headerLen) != headerLen) return 1;
+	if(len > 0 && write(sockfd, data, len) != (ssize_t)len) return 1;
+	if(write(sockfd, "\r\n", 2) != 2) return 1;
 
-    return 0;
+	return 0;
 }
 
 // Gerar HTML com a listagem de arquivos e enviar pelo socket
 int listDirectory(int sockfd, char *host, char *baseDir, char *path){
-    DIR *dir;
-    struct dirent *entry;
-    char buffer[BUFMAX];
+	DIR *dir;
+	struct dirent *entry;
+	char buffer[BUFMAX];
 
-    snprintf(buffer, BUFMAX, "%s/%s", baseDir, path);
-    dir = opendir(buffer);
-    if(!dir){
-        perror("opendir failed");
-        return 1;
-    }
+	snprintf(buffer, BUFMAX, "%s/%s", baseDir, path);
+	dir = opendir(buffer);
+	if(!dir){
+		perror("opendir failed");
+		return 1;
+	}
 
-    // Cabeçalho HTTP
-    ssize_t headerLen = snprintf(buffer, BUFMAX,
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Transfer-Encoding: chunked\r\n"
-        "\r\n"
-    );
+	// Cabeçalho HTTP
+	ssize_t headerLen = snprintf(buffer, BUFMAX,
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"Transfer-Encoding: chunked\r\n"
+		"\r\n"
+	);
 
-    if(write(sockfd, buffer, headerLen) != headerLen){
-        closedir(dir);
-        close(sockfd);
-        return 1;
-    }
+	if(write(sockfd, buffer, headerLen) != headerLen){
+		closedir(dir);
+		close(sockfd);
+		return 1;
+	}
 
-    // Chunk do cabeçalho HTML
-    ssize_t len = snprintf(buffer, BUFMAX,
-        "<!DOCTYPE html><html>\r\n"
-        "<style>table {margin-left: auto; margin-right: auto; width: 50%%;}</style>\r\n"
-        "<body>\r\n"
-        "<h1>Index of <i>/%s</i></h1>\r\n"
-        "<table>\r\n",
-        path
-    );
+	// Chunk do cabeçalho HTML
+	ssize_t len = snprintf(buffer, BUFMAX,
+		"<!DOCTYPE html><html>\r\n"
+		"<style>table {margin-left: auto; margin-right: auto; width: 50%%;}</style>\r\n"
+		"<body>\r\n"
+		"<h1>Index of <i>/%s</i></h1>\r\n"
+		"<table>\r\n",
+		path
+	);
 
-    if(sendChunk(sockfd, buffer, len)){
-        closedir(dir);
-        close(sockfd);
-        return 1;
-    }
+	if(sendChunk(sockfd, buffer, len)){
+		closedir(dir);
+		close(sockfd);
+		return 1;
+	}
 
-    // Entradas de diretório
-    while((entry = readdir(dir))){
-        if(!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
-            continue;
+	// Entradas de diretório
+	while((entry = readdir(dir))){
+		if(!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
+			continue;
 
-        char filename[BUFMAX];
-        char uri[BUFMAX];
-        char encodedUri[BUFMAX];
+		char filename[BUFMAX];
+		char uri[BUFMAX];
+		char encodedUri[BUFMAX];
 
-        snprintf(filename, BUFMAX, "%s/%s%s", baseDir, path, entry->d_name);
-        snprintf(uri, BUFMAX, "%s%s", path, entry->d_name);
+		snprintf(filename, BUFMAX, "%s/%s%s", baseDir, path, entry->d_name);
+		snprintf(uri, BUFMAX, "%s%s", path, entry->d_name);
 
-        if(urlEncode(uri, encodedUri, BUFMAX)){
-            fprintf(stderr, "Error: Failed to encode URI.\n");
-            closedir(dir);
-            close(sockfd);
-            return 1;
-        }
+		if(urlEncode(uri, encodedUri, BUFMAX)){
+			fprintf(stderr, "Error: Failed to encode URI.\n");
+			closedir(dir);
+			close(sockfd);
+			return 1;
+		}
 
-        struct stat statbuf;
-        if(stat(filename, &statbuf) == -1){
-            perror(filename);
-            closedir(dir);
-            close(sockfd);
-            return 1;
-        }
+		struct stat statbuf;
+		if(stat(filename, &statbuf) == -1){
+			perror(filename);
+			closedir(dir);
+			close(sockfd);
+			return 1;
+		}
 
-        len = snprintf(buffer, BUFMAX,
-            "<tr>\r\n"
-            "<td><a href=\"http://%s/%s\">%s</a></td>"
-            "<td>%s</td>"
-            "<td>%.2f KiB</td>\r\n"
-            "</tr>\r\n",
-            host,
-            encodedUri,
-            entry->d_name,
-            ctime(&statbuf.st_mtime),
-            statbuf.st_size / 1024.f
-        );
+		len = snprintf(buffer, BUFMAX,
+			"<tr>\r\n"
+			"<td><a href=\"http://%s/%s\">%s</a></td>"
+			"<td>%s</td>"
+			"<td>%.2f KiB</td>\r\n"
+			"</tr>\r\n",
+			host,
+			encodedUri,
+			entry->d_name,
+			ctime(&statbuf.st_mtime),
+			statbuf.st_size / 1024.f
+		);
 
-        if(sendChunk(sockfd, buffer, len)){
-            closedir(dir);
-            close(sockfd);
-            return 1;
-        }
-    }
+		if(sendChunk(sockfd, buffer, len)){
+			closedir(dir);
+			close(sockfd);
+			return 1;
+		}
+	}
 
-    // Rodapé HTML
-    const char *footer = "</table></body></html>\r\n";
-    if(sendChunk(sockfd, footer, strlen(footer))){
-        closedir(dir);
-        close(sockfd);
-        return 1;
-    }
+	// Rodapé HTML
+	const char *footer = "</table></body></html>\r\n";
+	if(sendChunk(sockfd, footer, strlen(footer))){
+		closedir(dir);
+		close(sockfd);
+		return 1;
+	}
 
-    // Chunk final
-    write(sockfd, "0\r\n\r\n", 5);
+	// Chunk final
+	write(sockfd, "0\r\n\r\n", 5);
 
-    closedir(dir);
-    close(sockfd);
-    return 0;
+	closedir(dir);
+	close(sockfd);
+	return 0;
 }
 
 //Se o endereço da página não termina com "/", redirecionar
